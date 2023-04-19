7cc6a2695d8a7f103b471352843a9d731822c728
Ademola Olatunbosun <ademomo11@gmail.com>
2023-04-19 01:34:15 +0000

    this is pyton

--- Code 01.py
+++ Code 01.py
@@ -4,6 +4,12 @@
 # Date of latest revision: 18 April 2023
 # Purpose: Import libraries
 
+
+# Transmit a single ICMP (ping) packet to a specific IP every two seconds.
+# Evaluate the response as either success or failure.
+# Assign success or failure to a status variable.
+# For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
+
 import os
 import time
 from datetime import datetime
